89aec61e1137cfb1f574c2c4068a49105d24c649
SankariMano <sankarimano97@gmail.com>
2017-08-01 15:49:15 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-public class Fact {
-public static void main(String args[]) 
+public class Fact {public static void main(String args[]) 
 {
 Scanner s=new Scanner(System.in);
 Integer a=s.nextInt();
+System.out.println("Enter number:"+a);
 if(a==0)
 {
 	System.out.println(1);
