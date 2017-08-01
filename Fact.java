b1f98847652a961e6e7a47b1fef90bf0dca6ce54
SankariMano <sankarimano97@gmail.com>
2017-08-01 15:47:27 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Fact {
+public static void main(String args[]) 
+{
+Scanner s=new Scanner(System.in);
+Integer a=s.nextInt();
+if(a==0)
+{
+	System.out.println(1);
+}else
+{
+int b=1;
+for(int i=1;i<=a;i++)
+{
+	b=b*i;
+}
+
+System.out.println(b);
+}
+}
+}
